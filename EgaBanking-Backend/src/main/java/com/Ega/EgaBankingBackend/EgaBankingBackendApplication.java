6cbda5d4484de044ba8f8f025782f0d6db593fcd
Drichdev <rich10dev@gmail.com>
2024-01-29 13:07:20 +0100

    initial commit

---
+++ EgaBanking-Backend/src/main/java/com/Ega/EgaBankingBackend/EgaBankingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Ega.EgaBankingBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgaBankingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgaBankingBackendApplication.class, args);
+	}
+
+}
---
+++ EgaBanking-Backend/src/test/java/com/Ega/EgaBankingBackend/EgaBankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ega.EgaBankingBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgaBankingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
